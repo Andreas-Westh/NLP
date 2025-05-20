5115766a8861d9c4319e2bfe287b74228279662c
60gCreatine <cphah643@gmail.com>
2025-05-20 16:03:10 +0200

    answered mock questions from teacher

--- R/examprep/Priest.R
+++ R/examprep/Priest.R
@@ -13,29 +13,6 @@ library(tidyverse)
 # 3) bruger flest kristne termer
 
 # loop
-url_kathrine_lilleoers_praediken_juledag="https://www.dansketaler.dk/praedikener/tale/kathrine-lilleoers-praediken-juledag-2018"
-url_kirsten_jorgensens_praediken_juledag="https://www.dansketaler.dk/praedikener/tale/kirsten-jorgensens-praediken-juledag"
-url_nikolaj_hartung_kjaerbys_praediken_juledag="https://www.dansketaler.dk/praedikener/tale/nikolaj-hartung-kjaerbys-praediken-juledag"
-url_alex_vestergaard_nielsens_praediken="https://www.dansketaler.dk/praedikener/tale/alex-vestergaard-nielsens-praediken-2-juledag"
-url_henrik_hojlunds="https://www.dansketaler.dk/praedikener/tale/henrik-hojlunds-praediken-juledag"
-url_peter_sanders_praediken="https://www.dansketaler.dk/praedikener/tale/peter-sanders-praediken-2-juledag"
-url_jesper_stanges_praediken="https://www.dansketaler.dk/praedikener/tale/jesper-stanges-praediken-2-juledag"
-url_lisbet_kjaer_mullers_praediken="https://www.dansketaler.dk/praedikener/tale/lisbet-kjaer-mullers-praediken-2-juledag"
-
-urls=c(url_alex_vestergaard_nielsens_praediken,url_henrik_hojlunds,url_jesper_stanges_praediken,
-       url_kathrine_lilleoers_praediken_juledag, url_kirsten_jorgensens_praediken_juledag, url_lisbet_kjaer_mullers_praediken,
-       url_nikolaj_hartung_kjaerbys_praediken_juledag, url_peter_sanders_praediken)
-
-all_texts <- data_frame(url=NA,text=NA)
-for (url in urls) {
-  print(url)
-  mtest=read_html(url)
-  tagfortale=".speech-article-content"
-  tale=mtest %>% html_node(tagfortale) %>% html_text()
-  rbind(all_texts,tale)
-}
-
-
 url="https://www.dansketaler.dk/praedikener/tale/kathrine-lilleoers-praediken-juledag-2018"
 url="https://www.dansketaler.dk/praedikener/tale/kirsten-jorgensens-praediken-juledag"
 url="https://www.dansketaler.dk/praedikener/tale/nikolaj-hartung-kjaerbys-praediken-juledag"
@@ -44,6 +21,49 @@ url="https://www.dansketaler.dk/praedikener/tale/henrik-hojlunds-praediken-juled
 url="https://www.dansketaler.dk/praedikener/tale/peter-sanders-praediken-2-juledag"
 url="https://www.dansketaler.dk/praedikener/tale/jesper-stanges-praediken-2-juledag"
 url="https://www.dansketaler.dk/praedikener/tale/lisbet-kjaer-mullers-praediken-2-juledag"
+
+# MAKE REGEX to get names
+
+urls=c(url_alex_vestergaard_nielsens_praediken,url_henrik_hojlunds,url_jesper_stanges_praediken,
+       url_kathrine_lilleoers_praediken_juledag, url_kirsten_jorgensens_praediken_juledag, url_lisbet_kjaer_mullers_praediken,
+       url_nikolaj_hartung_kjaerbys_praediken_juledag, url_peter_sanders_praediken)
+
+all_texts <- data_frame(url=NULL,text=NULL)
+for (url in urls) {
+  print(url)
+  mtest=read_html(url)
+  tagfortale=".speech-article-content"
+  tale=mtest %>% html_node(tagfortale) %>% html_text()
+  tale_df <- data_frame(url=url,text=tale)
+  all_texts <- rbind(all_texts,tale_df)
+}
+
+# question from teach
+# make an empty list with each url
+tom <- list()
+for (i in 1:length(urls)){
+  tom[i] = urls[i]
+  print(urls[i])
+}
+class(tom) # check its class
+
+# question from teach
+# make a df with top 10 used words from each priest in as few lines
+
+all_texts$præst <- str_extract(all_texts$url, "(?<=tale/).*")
+all_texts$præst <- str_extract(all_texts$præst, "^.*(?=praediken)")
+all_texts$præst <- gsub("-", " ", all_texts$præst)
+all_texts$præst <- trimws(all_texts$præst)
+tale_tk_w=unnest_tokens(tbl=all_texts,output = "text",input = text, token = "words",to_lower = T)
+tale_tk_w_sub= tale_tk_w %>% filter(!text %in% dkstop)
+
+top <- tale_tk_w_sub %>% 
+  group_by(præst) %>% count(text) %>% 
+  top_n(500)
+df <- split(top, top$præst)
+
+common_all <- reduce(df, inner_join, by = "text")
+
 
 # FOR HVER TALE....
 # hent talen vha rvest
@@ -109,6 +129,9 @@ tale_tk_w_sub= tale_tk_w %>% filter(!text %in% dkstop)
   kristine_df$priest = "lisbet-kjaer-mullers"
 
   
+  mean_sent <- taler_tk_s %>% group_by(præst) %>% mutate(mean = mean(score))
+  mean_grouped <- mean_sent %>% select(præst,mean) %>% group_by(præst) %>% distinct()
+  
   
   
   
@@ -117,12 +140,14 @@ tale_tk_w_sub= tale_tk_w %>% filter(!text %in% dkstop)
   priest_count <- total_real_priests %>% group_by(priest) %>% 
     count(text) %>% mutate(total = n) %>% ungroup()
   
+  gud <- priest_count %>% filter(text == "gud" | text == "guds")
+  
   priest_count <- total_real_priests %>% count(priest, text) %>% 
     group_by(priest) %>% mutate(total = sum(n)) %>% 
     ungroup()
   
-  priest_total <- priest_count %>% select(priest, total) %>% distinct(priest, .keep_all = T)
   # SAML resultaterne i én dataframe så præsterne kan sammenlignes
+  priest_total <- priest_count %>% select(priest, total) %>% distinct(priest, .keep_all = T)
   
   
   
