1cc47dc8cc71dddf977bcb606c4c9eddd65a6781
60gCreatine <cphah643@gmail.com>
2025-05-24 00:19:16 +0200

    optimized speech scrape loop

--- R/examprep/QueensSpeeches.R
+++ R/examprep/QueensSpeeches.R
@@ -9,31 +9,28 @@ library(stm)
 library(quanteda)
 
 
+# get urls
+URL_site <- "https://www.kongehuset.dk/monarkiet-i-danmark/nytaarstaler/#laes-de-seneste-nytaarstaler"
+url <- read_html(URL_site)
+links <- url %>% html_nodes(".accordion__container__item__content .field-item a") %>% 
+  html_attr("href") %>% as.data.frame()
+links <- links[-1,]
+
+
+
+
 # get speeches
-base_url <- "https://www.kongehuset.dk/monarkiet-i-danmark/nytaarstaler/nytaarstale-"
-base_url_upper <- "https://www.kongehuset.dk/nyheder/hendes-majestaet-dronningens-nytaarstale-"
-years <- 2001:2023
+base_url <- "https://www.kongehuset.dk"
 
+all_speeches <- data_frame(url=NULL, title = NULL, text=NULL)
 
-
-title <- read_html(loop_url) %>% html_element(".headline") %>% html_text()
-content <- read_html(loop_url) %>% html_element(".pad") %>% html_text()
-
-# scrape loop
-
-all_speeches <- data_frame(url=NULL, year = NULL, title = NULL, text=NULL)
-
-for (year in years) {
-  if (year > 2010) {
-    base_url = "https://www.kongehuset.dk/nyheder/hendes-majestaet-dronningens-nytaarstale-"
-  } else {
-    base_url = "https://www.kongehuset.dk/monarkiet-i-danmark/nytaarstaler/nytaarstale-"
-  }
+for (link in links) {
   Sys.sleep(runif(1, min = 0.2, max = 0.6))
-  print(paste0(year, loop_url))
-  loop_url <- paste0(base_url,year)
-  tmp_title <- read_html(loop_url) %>% html_element(".headline") %>% html_text()
-  tmp_content <- read_html(loop_url) %>% html_element(".pad") %>% html_text()
-  tmp_df <- data_frame(url = loop_url, year = year, title = tmp_title, text = tmp_content)  
+  print(link)
+  loop_url <- paste0(base_url,link) # match base url with scraped speech url
+  tmp_year <- str_extract(loop_url,"[0-9]{4}") # reGex out the year
+  tmp_content <- read_html(loop_url) %>% html_element(".rich-text") %>% html_text() # get the speech
+  tmp_df <- data_frame(url = loop_url, year = tmp_year, text = tmp_content)  
   all_speeches<- rbind(all_speeches,tmp_df)
 }
+
