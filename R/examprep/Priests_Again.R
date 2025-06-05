f18aadd9987c9b27d8fa06dd4575a5088d00eed2
60gCreatine <cphah643@gmail.com>
2025-06-05 10:17:16 +0200

    to do list

---
+++ R/examprep/Priests_Again.R
@@ -0,0 +1,29 @@
+# data retrieval ----
+
+
+# data prep ----
+# tokennize
+    # version with chapter´syntax and such
+
+# stopwords
+
+
+#### Tidy ####
+# count
+# wordcloud
+# normal ggplot
+
+# Zipf law?
+
+
+
+# topic modelling
+# document term matrix
+# if_idf
+
+
+
+# bigrams
+  # example with split by gender
+# relationsgrafer
+# korrelede ord
