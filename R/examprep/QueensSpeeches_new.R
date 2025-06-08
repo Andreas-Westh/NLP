39a2965a17930ebddccdf9d50bf993a5ca6adbc7
60gCreatine <cphah643@gmail.com>
2025-06-08 15:01:31 +0200

    made it seamless with spaCyr

--- R/examprep/QueensSpeeches_new.R
+++ R/examprep/QueensSpeeches_new.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(rvest)
 library(stringr)
 library(tidytext)
+library(topicmodels)
 library(stopwords)
 library(wordcloud2)
 library(ggplot2)
@@ -12,14 +13,13 @@ library(quanteda.textstats)
 library(Sentida)
 
 #### Data Retrieval ####
+##### Scraping #####
 # get urls
 URL_site <- "https://www.kongehuset.dk/monarkiet-i-danmark/nytaarstaler/#laes-de-seneste-nytaarstaler"
 url <- read_html(URL_site)
 links <- url %>% html_nodes(".accordion__container__item__content .field-item a") %>% 
   html_attr("href") %>% as.data.frame()
 links <- links[-1,]
-
-
 
 
 # get speeches
@@ -37,9 +37,35 @@ for (link in links) {
   all_speeches<- rbind(all_speeches,tmp_df)
 }
 
+##### .txt files #####
 
-#### Tidy ####
-raw_tokens <- all_speeches %>%
+
+#### Tokennize ####
+library(reticulate)
+Sys.setenv(RETICULATE_PYTHON = "/opt/anaconda3/envs/spacy/bin/python")
+library(spacyr)
+spacy_initialize(
+  model            = "da_core_news_md",
+  refresh_settings = TRUE,
+  verbose          = TRUE
+)
+
+spacy_df <- spacy_parse(all_speeches$text)
+spacy_df <- spacy_df %>% 
+  filter(lemma == str_extract(lemma, regex("[a-zæøåA-ZÆØÅ]+$")))
+
+# make doc_id more redable, in this example grouped by priest
+doc_ids <- unique(spacy_df$doc_id)
+better_ids <- unique(all_speeches$year)
+spacy_df <- spacy_df %>% 
+  mutate(doc_id = better_ids[match(doc_id, doc_ids)]) %>% 
+  rename(word = lemma)
+spacy_df$year <- spacy_df$doc_id
+
+raw_tokens_spacy <- spacy_df
+
+
+raw_tokens_tidy <- all_speeches %>%
   unnest_tokens(sentence, text, token = "sentences") %>%
   group_by(year) %>%
   mutate(sentence_number = row_number()) %>% # this first part was done to get sentence number for each word, for timeline plot or smth
@@ -47,10 +73,29 @@ raw_tokens <- all_speeches %>%
   unnest_tokens(word, sentence, token = "words") %>%
   select(url, year, sentence_number, word)
 
+
+###########################
+#### PICK SPACY OR TIDY ###
+###########################
+
+raw_tokens <- raw_tokens_tidy
+raw_tokens <- raw_tokens_spacy
+
+
+
+
+
+
+
+
+
+
 #remove stop words
-dkstop <- c(stopwords("da"),"kan","så","må","ved")
+dkstop <- c(stopwords("da"),"kan","så","må","ved","al")
 tokens <- raw_tokens %>% 
   filter(!word %in% dkstop)
+
+
 
 ##### count #####
 # total count per word
@@ -68,7 +113,7 @@ tokens_count <- tokens %>%
   left_join(total_count, by = "word") %>% 
   mutate(global_percent = round(global_total / sum(global_total) * 100,4))
 
-##### wordcloud #####
+###### wordcloud ######
 wc_data <- total_count %>% 
   rename(n = global_total) %>% 
   select(word, n)
@@ -114,27 +159,171 @@ sentiments_df %>%
              color = "darkgray") +
   labs(title = "the sentiment has become more positive over time")
 
+###### Sentiment distribution ######
+sentiments_df %>% 
+  ggplot(aes(x = score)) +
+  geom_histogram(binwidth = 0.05, fill = "steelblue", color = "white") +
+  geom_vline(xintercept = mean(sentiments_df$score), 
+             linetype = "dashed", color = "darkgray") +
+  labs(title = "Sentiment distribution",
+       x = "Mean sentiment score",
+       y = "Number of speeches") +
+  theme_minimal()
 
+##### Word and document frequency #####
+
+###### Zips law ######
 
 ##### Lix score #####
+mylix <- function(text){
+  words = unlist(str_split(text, " ")) # split text after a space
+  lixscore = round(mean(nchar(words)),2)
+  return(lixscore)
+}
 
+all_speeches <- all_speeches %>% 
+  rowwise() %>% 
+  mutate(lix_score = mylix(text))
 
 
 
 ##### Topics #####
+topic_sw <- c("kan", "så", "må", "ved", "al",
+              "danmark", "år", "godt", "danske", "vore", "vores", "nye", 
+              "aften", "nytår", "hele", "samfund", "sammen", "ønsker", 
+              "tak", "helt", "tid", "går", "både", "andre", "verden")
+topic_tokens <- tokens %>% filter(!word %in% topic_sw & 
+                                          word == str_extract(word, regex("^[a-zæøåA-ZÆØÅ]+$")))
+
+tidy_tokens_clean <- topic_tokens %>% count(year, word, sort = T) %>% 
+  bind_tf_idf(word, year, n) %>% 
+  group_by(year) %>% 
+  ungroup() %>% 
+  mutate(word = reorder(word, tf_idf))
+#tf (term freq) = how often a word is said in a document (%)
+#idf (inverse doc freq) = how rare a word is across all docs
+#tf_idf(tf * idf) = high if a word is important in one document, less in others
+
+###### unique words within each speech ######
+tidy_tokens_clean %>%
+  group_by(year) %>%
+  slice_max(tf_idf, n = 5, with_ties = FALSE) %>%
+  ungroup() %>%
+  mutate(word = reorder_within(word, tf_idf, year)) %>%
+  ggplot(aes(word, tf_idf, fill = year)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~year, scales = "free") +
+  coord_flip() +
+  scale_x_reordered()
+
+
+# make into data term matrix
+speeches_dtm <- tidy_tokens_clean %>% 
+  cast_dtm(year, word, n)
+speeches_dtm
+
+# use LDA to make the topics
+speeches_lda <- LDA(speeches_dtm, k = 4, control = list(seed = 1980))
+speeches_lda
+
+###### beta ######
+# beta = which word relates to which topic
+speeches_topic <- tidy(speeches_lda, matrix = "beta")
+speeches_topic
+# one-topic-per-word probability
+
+top_terms <- speeches_topic %>% 
+  group_by(topic) %>% 
+  slice_max(beta, n = 5) %>% 
+  ungroup() %>% 
+  arrange(topic, -beta)
+top_terms
+
+top_terms %>% 
+  mutate(term = reorder_within(term, beta, topic)) %>% 
+  ggplot(aes(beta, term, fill = factor(topic))) +
+  geom_col(show.legend = F) +
+  facet_wrap(~topic, scales = "free") +
+  scale_y_reordered()
+
+###### gamma ######
+# gamma = which topics relate to which document
+speeches_gamma <- tidy(speeches_lda, matrix = "gamma")
+speeches_gamma
+# 2006 gemma = 1.00 for topic 1, meaning it is purely filled by that topic
+speeches_gamma %>% 
+  mutate(speech = document, gamma * topic) %>% 
+  ggplot(aes(factor(topic), gamma)) +
+  geom_boxplot() +
+  facet_wrap(~speech) +
+  labs(x = "topic", y = expression(gamma))
+
+
+# this finds the dominant topic within each speech
+speeches_classifications <- speeches_gamma %>%
+  group_by(document) %>%
+  slice_max(gamma) %>% # this finds the highest gamma topic
+  ungroup()
+
+speeches_classifications
+
+
+speech_topics <- speeches_classifications %>%
+  count(document, topic) %>%
+  group_by(document) %>%
+  slice_max(n, n = 1) %>% 
+  ungroup() %>%
+  transmute(consensus = document, topic)
+
+speeches_classifications %>%
+  inner_join(speech_topics, by = "topic") %>%
+  filter(document != consensus)
+# 2004 is the main example for topic 2, tho 2001 is also fully topic 2
+# therefore they are both very much alike
+# seems like the clustering did quite well
+
+assignments <- augment(speeches_lda, data = speeches_dtm)
+assignments
+# here 'år' is assigned mutiple topics, why is that?
+# The same word can belong to different topics across documents because 
+# LDA assigns topics per word occurrence, not per word globally.
+
+
+# innerjoin with topics, to get the consensus 
+assignments <- assignments %>%
+  inner_join(speech_topics, by = c(".topic" = "topic"))
+assignments
+
+# plot into a CM
+library(scales)
+assignments %>%
+  count(document, consensus, wt = count) %>%
+  mutate(across(c(document, consensus), ~str_wrap(., 20))) %>%
+  group_by(document) %>%
+  mutate(percent = n / sum(n)) %>%
+  ggplot(aes(consensus, document, fill = percent)) +
+  geom_tile() +
+  scale_fill_gradient2(high = "darkred", label = percent_format()) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        panel.grid = element_blank()) +
+  labs(x = "Speeches words were assigned to",
+       y = "Speeches words came from",
+       fill = "% of assignments")
+# here it seems quite messy, but that is due to the topic overlap, general language, and all being new years speeches
 
 
 ##### Bigrams #####
+# chapter 4
 
 ###### specifik bigrams (like genderered) ######
 
-##### Zips law #####
 
 
 
 
 
-#### SpaCy ####
+#### SpaCy specific ####
 
 ##### Parts of speech #####
 
