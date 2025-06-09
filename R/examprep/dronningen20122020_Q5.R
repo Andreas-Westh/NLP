d89878dd442d25daba8d18f0f722d203e84d1a6c
60gCreatine <cphah643@gmail.com>
2025-06-10 01:47:00 +0200

    fixed small mistake with spacy vector naming

--- R/examprep/dronningen20122020_Q5.R
+++ R/examprep/dronningen20122020_Q5.R
@@ -43,23 +43,43 @@ spacy_initialize(
   verbose          = TRUE
 )
 
-spacy_df <- spacy_parse(all_speeches$text)
-spacy_df <- spacy_df %>% 
-  filter(lemma == str_extract(lemma, regex("[a-zæøåA-ZÆØÅ]+$")))
+texts <- all_speeches$text
+names(texts) <- all_speeches$year
 
-# make doc_id more redable, in this example grouped by priest
-doc_ids <- unique(spacy_df$doc_id)
-better_ids <- unique(all_speeches$year)
+spacy_df <- spacy_parse(texts)
 spacy_df <- spacy_df %>% 
-  mutate(doc_id = better_ids[match(doc_id, doc_ids)]) %>% 
-  rename(word = lemma)
-spacy_df$year <- spacy_df$doc_id
+  filter(lemma == str_extract(lemma, regex("[a-zæøåA-ZÆØÅ]+$"))) %>% 
+  mutate(word = lemma)
 
 raw_tokens_spacy <- spacy_df
 raw_tokens <- raw_tokens_spacy
 
+#remove stop words
+dkstop <- c(stopwords("da"),"kan","så","må","ved","al")
+tokens <- raw_tokens %>% 
+  filter(!word %in% dkstop)
+
+##### count #####
+# total count per word
+total_count <- tokens %>% 
+  count(word, name = "global_total", sort = T)
 
 
+# count grouped by priest
+tokens_count <- tokens %>% 
+  group_by(doc_id) %>% 
+  count(word, sort = T) %>% 
+  mutate(total = sum(n),
+         percent = round(n / total * 100,1)) %>% 
+  ungroup() %>% 
+  left_join(total_count, by = "word") %>% 
+  mutate(global_percent = round(global_total / sum(global_total) * 100,4))
+
+###### wordcloud ######
+wc_data <- total_count %>% 
+  rename(n = global_total) %>% 
+  select(word, n)
+wordcloud2(data = head(wc_data,100), size = 0.5)
 
 zipf_df <- raw_tokens %>%
   count(word, sort = TRUE) %>%
@@ -84,7 +104,7 @@ freq_by_rank <- raw_tokens %>%
 
 freq_by_rank %>% 
   ggplot(aes(rank, term_frequency, color = doc_id)) + 
-  geom_line(linewidth = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  geom_line(linewidth = 1.1, alpha = 0.8, show.legend = T) + 
   scale_x_log10() +
   scale_y_log10()
 
@@ -101,7 +121,7 @@ freq_by_rank %>%
   ggplot(aes(rank, term_frequency, color = doc_id)) + 
   geom_abline(intercept = lm$coefficients[1], slope = lm$coefficients[2], 
               color = "gray50", linetype = 2) +
-  geom_line(linewidth = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  geom_line(linewidth = 1.1, alpha = 0.8, show.legend = T) + 
   scale_x_log10() +
   scale_y_log10()
 
--- R/examprep/exam_dataprep_NLP.R
+++ R/examprep/exam_dataprep_NLP.R
@@ -32,20 +32,15 @@ spacy_initialize(
   verbose          = TRUE
 )
 
-spacy_df <- spacy_parse(all_speeches$text)
-spacy_df <- spacy_df %>% 
-  filter(lemma == str_extract(lemma, regex("[a-zæøåA-ZÆØÅ]+$")))
+texts <- all_speeches$text
+names(texts) <- all_speeches$year
 
-# make doc_id more redable, in this example grouped by priest
-doc_ids <- unique(spacy_df$doc_id)
-better_ids <- unique(all_speeches$doc_id)
+spacy_df <- spacy_parse(texts)
 spacy_df <- spacy_df %>% 
-  mutate(doc_id = better_ids[match(doc_id, doc_ids)]) %>% 
-  rename(word = lemma)
-spacy_df$year <- spacy_df$doc_id
+  filter(lemma == str_extract(lemma, regex("[a-zæøåA-ZÆØÅ]+$"))) %>% 
+  mutate(word = lemma)
 
 raw_tokens_spacy <- spacy_df
-
 raw_tokens <- raw_tokens_spacy
 
 
@@ -55,7 +50,7 @@ raw_tokens <- raw_tokens_spacy
 #remove stop words
 dkstop <- c(stopwords("da"),"kan","så","må","ved","al")
 tokens <- raw_tokens %>% 
-  filter(!word %in% dkstop)
+  filter(!lemma %in% dkstop)
 
 
 
@@ -80,3 +75,14 @@ wc_data <- total_count %>%
   rename(n = global_total) %>% 
   select(word, n)
 wordcloud2(data = head(wc_data,100), size = 0.5)
+
+
+
+
+
+
+
+
+
+
+
