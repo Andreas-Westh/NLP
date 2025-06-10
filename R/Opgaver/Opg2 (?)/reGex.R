39e96869696f411a855a533ca48f85723ddf7c67
60gCreatine <cphah643@gmail.com>
2025-06-10 17:02:44 +0200

    regex

---
+++ R/Opgaver/Opg2 (?)/reGex.R
@@ -0,0 +1,35 @@
+string <- "Fra 2020 til 2022 boede Kurt Verner i .nr 34"
+årstal_list <- str_extract_all(string, regex("[0-9]{4}+"))
+årstal <- årstal_list[[1]]
+
+
+#q2
+# read the html file, and retrieve 1 line and regex it to a working link
+raw_lines <- readLines("data/Q/Q2/edclinks.html", encoding = "UTF-8")
+library(rvest)
+html <- read_html(paste(raw_lines, collapse = "\n"))
+links <- html %>% 
+  html_nodes("meta[itemprop='url']") %>%
+  html_attr("content") %>% 
+  as.data.frame()
+
+base_url <- "https://www.edc.dk"
+paste0(base_url, links[1,1])
+
+# make a loop and save in a list
+full_links <- list()
+for (link in links[[1]]) {
+  tmp_link <- paste0(base_url,link)
+  full_links <- append(full_links, tmp_link)
+}
+
+full_links_df <- data.frame(links = as.character())
+for (i in 1:length(full_links)) {
+  tmp_link <- data.frame(links=full_links[[i]])
+  full_links_df <- rbind(full_links_df, tmp_link)
+}
+
+full_links_df <- full_links_df %>% 
+  rowwise() %>% 
+  mutate(sagnr = str_extract(links, "(?<=sagsnr=)[0-9]+"))
+
